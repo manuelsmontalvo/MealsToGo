43031a3ad193e722f49c7319f6b86286d31f9eb5
Manuel  Montalvo <manny@Manuels-MacBook-Pro.local>
2021-12-13 14:36:49 -0500

    card, styling

---
+++ src/features/restaurants/screens/restaurant.screen.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { StatusBar, View, SafeAreaView } from "react-native";
+import { Searchbar } from "react-native-paper";
+import styled from "styled-components/native";
+import { RestaurantInfo } from "../components/restaurant-info-card.component";
+
+const SafeArea = styled(SafeAreaView)`
+  flex: 1;
+  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
+`;
+
+const SearchContainer = styled(View)`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const RestaurantListContainer = styled(View)`
+  flex: 1;
+  background-color: ${(props) => props.theme.colors.bg.primary};
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+export const RestaurantsScreen = () => (
+  <SafeArea>
+    <SearchContainer>
+      <Searchbar />
+    </SearchContainer>
+    <RestaurantListContainer>
+      <RestaurantInfo />
+    </RestaurantListContainer>
+  </SafeArea>
+);
